be247a7a4500235a6d875716a41aa33e53130741
unknown <github2reza@gmail.com>
2025-03-09 01:26:34 -0800

    feat
    Developed endpoint for create service

--- MyPortfolio/src/routes/service/controller.js
+++ MyPortfolio/src/routes/service/controller.js
@@ -1,3 +1,11 @@
 import parentController from "../controller.js";
+import _ from "lodash";
 
-export default new (class extends parentController {})();
+export default new (class extends parentController {
+  async createService(req, res) {
+    const data = _.pick(req.body, ["title", "description"]);
+    const service = await new this.Service(data);
+    await service.save();
+    return this.response({ res, message: "Service Created", data: service });
+  }
+})();
--- MyPortfolio/src/routes/service/index.js
+++ MyPortfolio/src/routes/service/index.js
@@ -1,5 +1,14 @@
 import e from "express";
+import controller from "./controller.js";
+import validator from "./validator.js";
 
-const app = e.Router()
+const app = e.Router();
 
-export default app
+app.post(
+  "/",
+  validator.createServiceValidator(),
+  controller.validate,
+  controller.createService
+);
+
+export default app;

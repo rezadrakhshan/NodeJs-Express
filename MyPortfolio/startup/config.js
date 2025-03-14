d589d1ab34584db71bea3b5cabe9f0ef6f6159e7
unknown <github2reza@gmail.com>
2025-03-14 11:57:26 -0700

    feat
    Set limit with express rate limit

--- MyPortfolio/startup/config.js
+++ MyPortfolio/startup/config.js
@@ -1,8 +1,14 @@
-import helmet from "helmet"
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
 export default function (app, e) {
-    app.use(helmet())
-    app.use(e.urlencoded({extended:true}))
-    app.use(e.json())
-    app.use(e.static("public"))
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  });
+  app.use(limiter);
+  app.use(helmet());
+  app.use(e.urlencoded({ extended: true }));
+  app.use(e.json());
+  app.use(e.static("public"));
 }
